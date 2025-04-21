bfee016858d29b5ef21a0ed97d123affcaf602e8
yutongshuxue <yutongshuxue@126.com>
2025-04-22 00:40:46 +0800

    设置状态栏为透明色

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
-import 'package:huawei_shop/pages/intro/page.dart';
 import 'package:huawei_shop/router/constant.dart';
 import 'package:huawei_shop/router/router.dart';
 
 void main(){
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent, // 透明状态栏
+      statusBarIconBrightness: Brightness.light, // 状态栏图标颜色（Dark=黑色，Light=白色）
+    ),
+  );
+
   runApp(MyApp());
 }
 
